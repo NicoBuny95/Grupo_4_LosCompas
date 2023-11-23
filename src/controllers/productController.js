f822e573c08ed3f0318fb84cc69a6beaf1f424d7
Ramiro Rivera <ramrivera7@gmail.com>
2023-11-23 18:03:08 -0300

    Edicion productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,15 +1,23 @@
 const productos = [{id: 1, name: 'Aceite Natura 900 ml',
 description: 'Tipo de aceite para cocina: Girasol. Marca: Natura. Tipo de envase: Botella. Volumen de la unidad: 900 ml. Es libre de gluten. Con fecha de vencimiento',
-image: 'home-aceite.webp', category: 'Aceites', price: '899.00',}]
+image: 'home-aceite.webp', category: 'Aceites', price: '899.00',},
+{id: 2, name: 'Leche Descremada La Serenisima 1 Lt',
+description: 'Tipo de Lácteo: Leche Descremada Larga Vida. Marca: La Serenisima. Tipo de envase: Cartón. Volumen de la unidad: 1 Lt. Con fecha de vencimiento.',
+image: 'leche-la-serenisima-larga-vida.png', category: 'Lácteos', price: '549.00',}]
+
 let productController = {
     detalle: (req, res) => {
         let idProducto = req.params.id;
         let producto = productos.filter((prod) => {return prod.id == idProducto})
         res.render('detalle', {title: 'Detalle de producto', css: '/css/detalle.css', producto});
-
     },
     addProduct: (req, res) => {
-        res.render('addProduct', {title: 'Agregar producto', css: '/css/detalle.css'});
+        res.render('addProduct', {title: 'Agregar producto', css: '/css/addProduct.css'});
+    },
+    editProduct: (req, res) => {
+        let idProductoE = req.params.id;
+        let productoE = productos.filter((prodE) => {return prodE.id == idProductoE})
+        res.render('editProduct', {title: 'Editar producto', css: '/css/addProduct.css', productoE: productoE});
     },
     carrito: (req, res) => {
         res.render('carrito', {title: 'Carrito', css:'/css/carrito.css'});
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/productController');
 router.get('/productDetail/:id', productController.detalle);
 router.get('/carrito', productController.carrito);
 router.get('/addProduct', productController.addProduct);
+router.get('/editProduct/:id', productController.editProduct)
 
 module.exports = router;
