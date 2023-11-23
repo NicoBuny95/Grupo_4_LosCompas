7b0523d048dd0bac00667ec63e767fb55fef32b6
Ramiro Marcelo Rivera <rrivera@local.jussantiago.gov.ar>
2023-11-23 12:05:28 -0300

    Detalle de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,18 @@
+const productos = [{id: 1, name: 'Aceite Natura 900 ml',
+description: 'Tipo de aceite para cocina: Girasol. Marca: Natura. Tipo de envase: Botella. Volumen de la unidad: 900 ml. Es libre de gluten. Con fecha de vencimiento',
+image: 'home-aceite.webp', category: 'Aceites', price: '899.00',}]
 let productController = {
     detalle: (req, res) => {
-        res.render('detalle', {title: 'Detalle de producto', css: 'css/detalle.css'});
+        let idProducto = req.params.id;
+        let producto = productos.filter((prod) => {return prod.id == idProducto})
+        res.render('detalle', {title: 'Detalle de producto', css: '/css/detalle.css', producto});
+
+    },
+    addProduct: (req, res) => {
+        res.render('addProduct', {title: 'Agregar producto', css: '/css/detalle.css'});
     },
     carrito: (req, res) => {
-        res.render('carrito', {title: 'Carrito', css:'css/carrito.css'});
+        res.render('carrito', {title: 'Carrito', css:'/css/carrito.css'});
     }
 }
 
