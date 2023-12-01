41980a5e6db37937aac659e1a1cf65a6b60a67a9
JorgeNeder97 <jorge.neder97@gmail.com>
2023-12-01 07:28:53 -0300

    Index.ejs

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+let productosFilePath = path.join(__dirname, '../../data/products.json');
+let archivo = fs.readFileSync(productosFilePath, {encoding: 'utf-8'});
+let productos = JSON.parse(archivo);
+
 let mainController = {
     index: (req, res) => {
-        res.render('index', {title: 'LC Supermercado', css:'css/index.css'});
+        res.render('index', {title: 'LC Supermercado', css:'css/index.css', productos: productos});
     },
 }
 
