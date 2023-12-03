ec77e0a39284d734003396e4b15f4c0874e417bf
JorgeNeder97 <jorge.neder97@gmail.com>
2023-12-03 17:53:36 -0300

    Avanzando con el login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,11 @@ let userController = {
     login: (req, res) => {
         res.render('login', {title: 'Login', css:'/css/login.css'});
     },
+    loging: (req, res) => {
+        let infoLog = req.body;
+
+        res.redirect('/');
+    },
     register: (req, res) => {
         res.render('register', {title: 'Registrarme', css:'/css/registrar.css'});
     },
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/login', userController.login);
+router.post('/login', userController.loging);
 router.get('/register', userController.register);
 
 module.exports = router;
