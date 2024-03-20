fcf235a3b9909c9aaf175812e6b327f21fb4ea5b
Nico Herrera <niconh912@gmail.com>
2024-03-20 11:20:39 -0300

    Correcion validacion login front

--- public/js/loginValidacion.js
+++ public/js/loginValidacion.js
@@ -1,33 +1,35 @@
-window.addEventListener('load',()=> {
-    //Capturas de elementos input
+window.addEventListener('load', function() {
+    // Capturas de elementos input
     var emailInput = document.getElementById('email');
     var passwordInput = document.getElementById('password');
-    //Captura de elemneto span para los mensajes de error
+    // Captura de elemento span para los mensajes de error
     var errorEmail = document.getElementById('error-email');
     var errorPassword = document.getElementById('error-password');
     // Reiniciar los mensajes de error
     errorEmail.innerHTML = '';
     errorPassword.innerHTML = '';
-    
+
     document.getElementById('login-form').addEventListener('submit', function(event) {
         event.preventDefault();
-        if (!errorName ){}
     });
-    
+
     // Validar el campo Email
     emailInput.addEventListener('blur', function() {
         if (validator.isEmpty(emailInput.value)) {
-            errorEmail.innerHTML = 'El email del usuario es un campo obligatorio.'
-        } else if (!validator.isEmail(emailInput.value)) {        
+            errorEmail.innerHTML = 'El email del usuario es un campo obligatorio.';
+        } else if (!validator.isEmail(emailInput.value)) {
             errorEmail.innerHTML = 'El email no es correcto.';
-        } else {errorEmail.innerHTML = ""} 
-    
-    })
+        } else {
+            errorEmail.innerHTML = "";
+        }
+    });
+
     // Validar el campo de Password 
     passwordInput.addEventListener('blur', function() {
-        if (validator.isEmpty(descriptionInput.value)) {
+        if (validator.isEmpty(passwordInput.value)) {
             errorPassword.innerHTML = 'EL password es un campo obligatorio.';
+        } else {
+            errorPassword.innerHTML = '';
         }
-        else {errorPassword.innerHTML = '';}
-    })    
+    });
 });
--- src/middlewares/validationFormLogin.js
+++ src/middlewares/validationFormLogin.js
@@ -1,25 +1,44 @@
-const { check } = require('express-validator');
-const db = require('../database/models'); 
-
-
-
+const { check, validationResult } = require('express-validator');
+const db = require('../database/models');
+const bcrypt = require('bcrypt');
 
 const validateLogin = [
-    check('email')
-                     .notEmpty().withMessage('El correo electrónico es obligatorio')
-                     .isEmail().withMessage('El correo electrónico debe ser válido')
-                     .custom(async (value) => {
-                         // Buscar un usuario con el correo electrónico proporcionado
-                         const user = await db.User.findOne({ where: { users_email: value } });
-                         
-                         if (!user) {
-                             throw new Error('El correo electrónico no se encuentra registrado');
-                         }
-                     }),
-    check('password').notEmpty().withMessage('La contraseña es obligatoria')
-                     .isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres')
-                     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
-                     .withMessage('La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial'),        
+  check('email')
+    .notEmpty().withMessage('El correo electrónico es obligatorio')
+    .isEmail().withMessage('El correo electrónico debe ser válido')
+    .custom(async (value) => {
+      try {
+        // Buscar el usuario en la base de datos
+        const user = await db.User.findOne({ where: { users_email: value } });
+        if (!user) {
+          throw new Error('El correo electrónico no se encuentra registrado');
+        }
+        return true;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }),
+  check('password')
+    .notEmpty().withMessage('La contraseña es obligatoria')
+    .custom(async (value, { req }) => {
+      try {
+        // Buscar el usuario en la base de datos
+        const user = await db.User.findOne({ where: { users_email: req.body.email } });
+        if (!user) {
+          throw new Error('El correo electrónico no se encuentra registrado');
+        }
+        // Comparar la contraseña proporcionada con la contraseña almacenada en la base de datos
+        const passwordMatch = await bcrypt.compare(value, user.users_password);
+        if (!passwordMatch) {
+          throw new Error('La contraseña no es correcta');
+        }
+        
+        req.user = user;
+        return true;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    })
 ];
 
 module.exports = validateLogin;
